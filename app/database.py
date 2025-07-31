b7868f571f8774d784545e609a6d4e63d00c0673
lavkush9051 <lavkush9051@gmail.com>
2025-07-31 12:24:18 +0530

    ready for deployment

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:admin@localhost:5432/face_db"
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:admin@localhost:5432/face_db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
